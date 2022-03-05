7a07a514dca5393df1057eeca2a8d15c912bba31
tonistorres <sisvenda2011@gmail.com>
2022-03-05 16:51:24 -0300

    feat: Ordenar registros de forma ascendente (ASC) implicita

---
+++ Basicos/17_Ordernar_Registros_de_forma_ASC_implicita.sql
@@ -0,0 +1,6 @@
+--  18 - Selecionar registros das colunas first_name, last_name do banco sakila da tabela actor
+--  ordenando os registro de forma implicita ASC primeiro first_name depois last_name
+
+SELECT first_name, last_name FROM actor 
+ORDER BY first_name, last_name 
+
