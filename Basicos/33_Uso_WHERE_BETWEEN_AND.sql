39d4854d10cd64dcb596c1a2574f1939b9a79f09
tonistorres <sisvenda2011@gmail.com>
2022-03-06 11:20:26 -0300

    feat: Uso particula WHERE e operadore BETWEEN trabalhando conjuntamente com a particula AND

---
+++ Basicos/33_Uso_WHERE_BETWEEN_AND.sql
@@ -0,0 +1,5 @@
+
+--  33 - Encontrar alugeis entre os dias 2005-05-26 00:00:00 e 2005-05-27 00:00:00:
+
+SELECT * FROM rental WHERE rental_date BETWEEN '2005-05-26 00:00:00' AND '2005-05-27 00:00:00'
+
