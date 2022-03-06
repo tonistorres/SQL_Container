c30722e6f475d73753cf8eb01fa0926a1557fc9c
tonistorres <sisvenda2011@gmail.com>
2022-03-06 16:08:10 -0300

    feat:Inserindo dados numa tabela com INSERT INTO

---
+++ Basicos/49_Utilizando_INSERT_INTO.sql
@@ -0,0 +1,9 @@
+
+-- Procurando Nemo foi aclamado pela crítica! Foi classificado em 6.8, fez 450 milhões no mercado interno e
+-- 370 milhões no mercado internacional.
+-- Adicione as informações à tabela BoxOffice:
+
+
+INSERT INTO Pixar.BoxOffice (movie_id, rating, domestic_sales, international_sales) VALUES
+(9, 6.8, 450000000, 370000000)
+
