3c77b1de90c8ab06ea1b075a8636ab8875146b0e
tonistorres <sisvenda2011@gmail.com>
2022-03-06 18:07:39 -0300

    feat: Uso da funcao DATEDIFF para calcular em dias a diferença entre uma data inicial e uma data final

---
+++ ComandoSQL/57_Uso_funcao_DATEDIFF.sql
@@ -0,0 +1,8 @@
+
+-- > 57 - Comando sql que tem como finalidade calcular em dias a diferença 
+-- > entre duas datas tendo o resultado positio quando data colocada 
+-- > da seguinte forma: Data_Final - Data_inicial
+-- > 30, ou seja, a primeira data é 30 dias depois da segunda
+
+
+SELECT DATEDIFF('2020-01-31', '2020-01-01');
