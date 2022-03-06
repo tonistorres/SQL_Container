29c7f85699bcd6b8d6bd362c154f38bb50e6f699
tonistorres <sisvenda2011@gmail.com>
2022-03-06 18:10:59 -0300

    feat:Uso da funcao TIMEDIFF calcular diferenca entre datas

---
+++ ComandoSQL/59_Uso_fucao_TIMEDIFF.sql
@@ -0,0 +1,7 @@
+
+-- > 59 -  Comando SQL que tem por deifinição retornar a diferença entre Datas 
+-- > quando comparadas
+-- > -239:00:00, ou seja, há uma diferença de 239 horas entre as datas
+
+
+SELECT TIMEDIFF('2021-08-11 08:30:10', '2021-08-01 09:30:10');
