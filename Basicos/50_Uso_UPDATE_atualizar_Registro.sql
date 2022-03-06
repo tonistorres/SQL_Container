2a1fe6f6d315e9b07a7ab33c889e5b725f1e38fd
tonistorres <sisvenda2011@gmail.com>
2022-03-06 16:10:33 -0300

    feat:Uso UPDATE para atualizar dados numa tabela

---
+++ Basicos/50_Uso_UPDATE_atualizar_Registro.sql
@@ -0,0 +1,8 @@
+
+
+--  50 - O diretor do filme "Procurando Nemo" está incorreto, na verdade ele foi dirigido por Andrew Staton.
+--  Corrija esse dado utilizando o UPDATE .
+
+
+UPDATE Movies SET director = 'Andrew Staton'
+WHERE id = 3;
