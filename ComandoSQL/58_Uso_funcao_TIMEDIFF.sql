1b695b89847316017fc583febb86713a99677adc
tonistorres <sisvenda2011@gmail.com>
2022-03-06 18:09:28 -0300

    feat:Uso da funcao TIMEDIFF para calcular a diferenca entre horas

---
+++ ComandoSQL/58_Uso_funcao_TIMEDIFF.sql
@@ -0,0 +1,7 @@
+
+-- > Comando SQL que tem por definição retornar a diferença entre 
+-- > dois tempos qualquer
+-- > -01:00:00, ou seja, há 1 hora de diferença entre os horários
+
+
+SELECT TIMEDIFF('08:30:10', '09:30:10');
