5a4c6938b6858fdf6dd73300f89ccebebfa449a9
tonistorres <sisvenda2011@gmail.com>
2022-03-06 10:49:31 -0300

    feat: Uso do WHERE com o Operador Logico OR

---
+++ Basicos/27_Uso_WHERE_OR.sql
@@ -0,0 +1,9 @@
+
+--  27 - Selecionar filmes 'AFFAIR PREJUDICE', 'AFRICAN EGG', 'AGENT TRUMAN', 'AIRPLANE SIERRA' (maneira não muito prática)
+	
+	SELECT title FROM film 
+	WHERE title = 'AFFAIR PREJUDICE'
+	OR title = 'AFRICAN EGG'
+	OR title = 'AGENT TRUMAN'
+	OR title = 'AIRPLANE SIERRA';
+	
