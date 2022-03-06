93f08056fa17bfb49461cbaa86085e0e02ed3136
tonistorres <sisvenda2011@gmail.com>
2022-03-06 11:48:40 -0300

    feat: Uso do comando INSERT INTO para adicionar mais de um registro

---
+++ Basicos/43_Uso_comando_INSERT_INTO_mais_de_um_registro.sql
@@ -0,0 +1,13 @@
+
+-- Inserindo mais de uma valor ao mesmo tempo em actor: 'Robbins Ferris' e 'Tim Souza':
+
+
+INSERT
+	INTO
+	actor (first_name,
+	last_name)
+VALUES 
+('Robbins', 'Ferris'),
+('Tim', 'Souza'),
+('Tim', 'Maia'),
+('Jose', 'de Souza');
