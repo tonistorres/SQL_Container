1fd1892c3cbe7dd7ace0e93daa0474de308df773
tonistorres <sisvenda2011@gmail.com>
2022-03-12 20:34:11 -0300

    Inserindo dados na tabela funcionario

---
+++ ComandoSQL/HCODE/8_inserindo_dados_tabela_funcionario.sql
@@ -0,0 +1,10 @@
+SELECT * FROM hcode.tb_funcionario;
+
+INSERT INTO hcode.tb_funcionario(nome,salario,adimissao,sexo,cadastro) 
+values
+('Dezuita Torres',1200,'2022-03-12','M',null),
+('Nadira Cruz',1200,'2022-03-12','M',null),
+('Eunado Gimenes',1200,'2022-03-12','M',null),
+('Tabita Silva',1200,'2022-03-12','M',null),
+('Mariana Goes',1200,'2022-03-12','M',null);
+
