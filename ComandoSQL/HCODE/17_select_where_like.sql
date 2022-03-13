7f631112217833390fa9af11bdca57629d03bfc0
tonistorres <sisvenda2011@gmail.com>
2022-03-12 21:55:56 -0300

    feat:SELECT WHERE LIKE

---
+++ ComandoSQL/HCODE/17_select_where_like.sql
@@ -0,0 +1,3 @@
+  #comentário: selecione todos da tabela funcionários quando nome contiver x sem importar em que posição se inicio ou fim ou meio
+  
+  select *from tb_funcionario where nome like 'm%'; 
